e6ff62a921e35d14967b2aa9952ee8cde76c37ac
ushchent <am@opendata.by>
2022-09-21 18:09:43 +0300

    Настроить поисковую выдачу

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 
 const hosts = {"local": "/:8000/", "web": "https://api.opendata.by",
 	"test": "https://test.nagrady.by"};
-const current_host = hosts["local"];
+const current_host = hosts["web"];
 
 // Состояние программы в части загрузки данных для отображение на карте
 const state = {
     "data": {},
     "markers": null,
 };
+
+const test_data = {};
 
 // Управление текстом в строке ввода адреса
 const name_field = document.getElementById("autocomplete");
@@ -33,12 +35,24 @@ function get_name(str) {
         fetch(`${current_host}/imena/search/${str}/`)
             .then(response => response.json())
             .then(data => {
-                    state.data = data;
+                    state.data.search_results = data;
                     create_result_list(data)
                 })
     }
 }
 
+function list_items_listeners(item, place_holder) {
+    var place = document.getElementById(place_holder);
+    var items = place.getElementsByTagName(item);
+    for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener("click", function() {
+            const name_selected = this.textContent;
+			get_data_by_name(name_selected);
+            document.getElementById("autocomplete").value = name_selected;
+            document.getElementById("results_list").className = "hidden";
+        });
+    }
+}
 function create_result_list(data) {
     const target = document.getElementById("results_list");
 
@@ -53,13 +67,13 @@ function create_result_list(data) {
         target.className = "";
     }
     for (var i = 0; i < data.length; i++) {
-        var address_text = document.createTextNode(data[i].address);
+        var fname_text = document.createTextNode(data[i].fname);
         var list_item = document.createElement("li");
-        list_item.appendChild(address_text);
+        list_item.appendChild(fname_text);
         list.appendChild(list_item);
     }
     target.appendChild(list);
-    bezvody_list_items_listeners("li", "results_list")
+    list_items_listeners("li", "results_list")
     document.getElementById("results_list")
         .style.border = "1px solid #A5ACB2";
 }
@@ -76,61 +90,113 @@ function add_markers(name_id) {
     mymap.addLayer(state.markers);
 }
 
-const get_data_by_name_id = (value) => {
-		let name_id = value.id;
-		console.log(name_id);
-		if (state.data.name_id == undefined) {
+
+const manage_layers = test_data => {
+	if (heatmapLayer._data.length == 0) {
+		console.log("First drawing...");
+		heatmapLayer.setData(test_data);
+	} else {
+		//heatmapLayer.remove();
+		heatmapLayer._data = []
+		heatmapLayer.setData(test_data);
+	}
+}
+
+const get_data_by_name = name => {
+		console.log(name);
+		if (state.data[name] == undefined) {
 			console.log("Loading data...")
-			fetch(`/data/${name_id}.json`)
+			fetch(`${current_host}/imena/fname/${name}`)
 				.then(response => response.json())
 				.then(data => {
-					state.data.name_id = data;
+					state.data[name] = data;
+					test_data["data"] = state.data[name];
+					//manage_layers(test_data);
 				})
-				.then(data => manage_markers(name_id))
+				.then(data => {
+					manage_layers(test_data)
+					})
 		} else {
 			console.log("Not loading data...")
-			manage_markers(name_id)
+			test_data["data"] = state.data[name];
+			manage_layers(test_data)
 		}
 	}
 
+var baseLayer = L.tileLayer(
+  'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution: '...',
+    maxZoom: 8
+  }
+);
 
-function create_marker_cluster_group(name_id) {
-    state.markers = L.markerClusterGroup({
-        showCoverageOnHover: false,
-        iconCreateFunction: function(cluster) {
-        const class_name = name_id + " marker-cluster";
-        return L.divIcon({ html: "<div><span>" + cluster.getChildCount() + "</span></div>",
-            className: class_name });
-        }
-    });
-}
+var cfg = {
+  // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+  // if scaleRadius is false it will be the constant radius used in pixels
+  "radius": 30,
+  "maxOpacity": .7,
+  // scales the radius based on map zoom
+  "scaleRadius": false,
+  // if set to false the heatmap uses the global maximum for colorization
+  // if activated: uses the data maximum within the current map boundaries
+  //   (there will always be a red spot with useLocalExtremas true)
+  "useLocalExtrema": false,
+  // which field name in your data represents the latitude - default "lat"
+  latField: 'lat',
+  // which field name in your data represents the longitude - default "lng"
+  lngField: 'lon',
+  // which field name in your data represents the data value - default "value"
+  valueField: 'count'
+};
 
-// Добавляем маркеры
-function manage_markers(name_id) {
-    if (state.markers) {
-        state.markers.clearLayers();
-        create_marker_cluster_group(name_id);
-        add_markers(name_id);
-    } else {
-        create_marker_cluster_group(name_id);
-        add_markers(name_id);
-    }
-}
 
-// Карта
-const map_icon = L.icon({
-  iconUrl: 'img/icon.png',
-  iconSize: [29, 24],
-  iconAnchor: [9, 21],
-  popupAnchor: [0, -14]
+var heatmapLayer = new HeatmapOverlay(cfg);
+
+var map = new L.Map('mapid', {
+  center: new L.LatLng(53.893009, 27.567444),
+  zoom: 7,
+  layers: [baseLayer, heatmapLayer]
 });
 
-const mymap = L.map('mapid', {
-                minZoom: 11,
-                maxZoom: 15
-                })
-                .setView([53.893009, 27.567444], 11);
 
-L.tileLayer('/tiles/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+
+//function create_marker_cluster_group(name_id) {
+    //state.markers = L.markerClusterGroup({
+        //showCoverageOnHover: false,
+        //iconCreateFunction: function(cluster) {
+        //const class_name = name_id + " marker-cluster";
+        //return L.divIcon({ html: "<div><span>" + cluster.getChildCount() + "</span></div>",
+            //className: class_name });
+        //}
+    //});
+//}
+
+// Добавляем маркеры
+//function manage_markers(name_id) {
+    //if (state.markers) {
+        //state.markers.clearLayers();
+        //create_marker_cluster_group(name_id);
+        //add_markers(name_id);
+    //} else {
+        //create_marker_cluster_group(name_id);
+        //add_markers(name_id);
+    //}
+//}
+
+// Карта
+//const map_icon = L.icon({
+  //iconUrl: 'img/icon.png',
+  //iconSize: [29, 24],
+  //iconAnchor: [9, 21],
+  //popupAnchor: [0, -14]
+//});
+
+//const mymap = L.map('mapid', {
+                //minZoom: 11,
+                //maxZoom: 15
+                //})
+                //.setView([53.893009, 27.567444], 11);
+
+//L.tileLayer('/tiles/{z}/{x}/{y}.png', {
+    //attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//}).addTo(mymap);
